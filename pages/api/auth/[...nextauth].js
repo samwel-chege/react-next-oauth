127e950d8d19b1bb148f08c9f89f9216ba281636
samwel-chege <samwelchegeh09@gmail.com>
2022-07-12 09:32:54 +0300

    include error handling

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,12 @@
+import NextAuth from "next-auth/next";
+import GithubProvider from "next-auth/providers/github";
+
+export default NextAuth({
+    providers: [
+        GithubProvider({
+            clientId: process.env.GITHUB_CLIENT_ID,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET,
+        }),
+        // more providers can be added here
+    ],
+});
--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,10 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
+import { useSession, signIn, signOut } from "next-auth/react"
+
 export default function Home() {
+  const {data: session } = useSession()
   return (
     <div className={styles.container}>
       <Head>
@@ -12,6 +15,9 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
+        {!session && (<button onClick={() => signIn()}>Sign in with Github</button>)}
+        {session && <button onClick={() => signOut()} >Sign out({session.user.name})</button> }
+
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
